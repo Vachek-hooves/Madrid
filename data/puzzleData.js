b20f7a99f39991337f35718f64c19ef4bb7c6562
Vachek-hooves <vachek0602@gmail.com>
2024-10-17 13:15:16 +0300

    create puzzle data set + images

---
+++ data/puzzleData.js
@@ -0,0 +1,43 @@
+export const puzzleData = [
+    {
+        id: 1,
+        image: require('../assets/image/history.png'),
+    },
+    {
+        id: 2,
+        image: require('../assets/image/culture.png'),
+    },
+    {
+        id: 3,
+        image: require('../assets/image/landmark.png'),
+    },
+    {
+        id: 4,
+        image: require('../assets/image/sport.png'),
+    },
+    {
+        id: 5,
+        image: require('../assets/image/gastronomy.png'),
+    },
+    {
+        id: 6,
+        image: require('../assets/image/festival.png'),
+    },      
+    {
+        id: 7,
+        image: require('../assets/image/famousPeople.png'),
+    },
+    {
+        id: 8,
+        image: require('../assets/image/architecture.png'),
+    },
+    {
+        id: 9,
+        image: require('../assets/image/transportation.png'),
+    },
+    {
+        id: 10,
+        image: require('../assets/image/parks.png'),
+    },
+    
+]
