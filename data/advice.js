7d1e82457cac7dba1fe9524bacaa8911d5722f54
Vachek-hooves <vachek0602@gmail.com>
2024-10-17 10:30:50 +0300

    update

--- data/advice.js
+++ data/advice.js
@@ -1,10 +1,62 @@
-export const advice = [{id:1,header:'Plan Your Itinerary in Advance:',advice:'Study the city map and create a list of attractions you want to visit, such as Plaza Mayor, the Prado Museum, Retiro Park, and the Royal Palace. Consider the distances between them and the possibility of visiting multiple places in one day.'},{
-    id:2,header:'Use Public Transportation:',advice:'Madrid has a well-developed network of metro and bus routes, making it easy and convenient to get around the city. Purchase a reusable “tarjeta de transporte” card, which will allow you to use all forms of transportation without hassle.'},{id:3,header:'Visit Local Markets:',advice:'Do not miss the opportunity to try local cuisine at famous markets like Mercado de San Miguel and Mercado de Antón Martín. These are great places to sample tapas, jamón, and local wines.'},
-    {id:4,header:'Know the Opening Hours of Attractions:',advice:'Many museums and historical sites have specific hours of operation and days when they are closed. For example, the Prado Museum offers free entry during certain hours, so it’s best to plan your visits ahead of time.'},
-    {id:5,header:'Learn About Local Culture and Customs: ',advice:'Familiarize yourself with traditions like the “siesta” (afternoon nap) and late dinners. In Madrid, dinner is often served after 9 PM, so consider this in your schedule.'},
-    {id:6,header:'Join Guided Tours:',advice:'If you want to gain a deeper understanding of the city’s history and culture, consider joining a guided tour. These could be walking tours, bike tours, or culinary routes that will take you to lesser-known but interesting places.'},
-    {id:7,header:'Visit Parks and Squares:',advice:'Madrid is known for its parks, such as Retiro Park and Casa de Campo. Spend time outdoors by taking a stroll or having a picnic to enjoy the city’s atmosphere.'},
-    {id:8,header:'Look for Free Events: ',advice:'Madrid frequently hosts free exhibitions, concerts, and events, especially during the summer months. Check the city’s cultural calendar so you don’t miss interesting happenings.'},
-    {id:9,header:'Be Open to Communication:',advice:'Madrileños are known for their hospitality and friendliness. Do not hesitate to ask questions, engage with locals, and learn about their opinions on the city and its culture.'},
-    {id:10,header:'Keep Memories: ',advice:'Take photos, keep a travel journal, or collect souvenirs. This will help you preserve the memories of your trip to Madrid and share them with friends and family.'},
-]
+export const advice = [
+  {
+    id: 1,
+    header: 'Plan Your Itinerary in Advance:',
+    advice:
+      'Study the city map and create a list of attractions you want to visit, such as Plaza Mayor, the Prado Museum, Retiro Park, and the Royal Palace. Consider the distances between them and the possibility of visiting multiple places in one day.',
+  },
+  {
+    id: 2,
+    header: 'Use Public Transportation:',
+    advice:
+      'Madrid has a well-developed network of metro and bus routes, making it easy and convenient to get around the city. Purchase a reusable “tarjeta de transporte” card, which will allow you to use all forms of transportation without hassle.',
+  },
+  {
+    id: 3,
+    header: 'Visit Local Markets:',
+    advice:
+      'Do not miss the opportunity to try local cuisine at famous markets like Mercado de San Miguel and Mercado de Antón Martín. These are great places to sample tapas, jamón, and local wines.',
+  },
+  {
+    id: 4,
+    header: 'Know the Opening Hours of Attractions:',
+    advice:
+      'Many museums and historical sites have specific hours of operation and days when they are closed. For example, the Prado Museum offers free entry during certain hours, so it’s best to plan your visits ahead of time.',
+  },
+  {
+    id: 5,
+    header: 'Learn About Local Culture and Customs: ',
+    advice:
+      'Familiarize yourself with traditions like the “siesta” (afternoon nap) and late dinners. In Madrid, dinner is often served after 9 PM, so consider this in your schedule.',
+  },
+  {
+    id: 6,
+    header: 'Join Guided Tours:',
+    advice:
+      'If you want to gain a deeper understanding of the city’s history and culture, consider joining a guided tour. These could be walking tours, bike tours, or culinary routes that will take you to lesser-known but interesting places.',
+  },
+  {
+    id: 7,
+    header: 'Visit Parks and Squares:',
+    advice:
+      'Madrid is known for its parks, such as Retiro Park and Casa de Campo. Spend time outdoors by taking a stroll or having a picnic to enjoy the city’s atmosphere.',
+  },
+  {
+    id: 8,
+    header: 'Look for Free Events: ',
+    advice:
+      'Madrid frequently hosts free exhibitions, concerts, and events, especially during the summer months. Check the city’s cultural calendar so you don’t miss interesting happenings.',
+  },
+  {
+    id: 9,
+    header: 'Be Open to Communication:',
+    advice:
+      'Madrileños are known for their hospitality and friendliness. Do not hesitate to ask questions, engage with locals, and learn about their opinions on the city and its culture.',
+  },
+  {
+    id: 10,
+    header: 'Keep Memories: ',
+    advice:
+      'Take photos, keep a travel journal, or collect souvenirs. This will help you preserve the memories of your trip to Madrid and share them with friends and family.',
+  },
+];
