92b7344997e9bfd3167c3b34fabb8f6f162f324c
Vachek-hooves <vachek0602@gmail.com>
2024-10-17 11:28:12 +0300

    updates

--- App.js
+++ App.js
@@ -3,15 +3,24 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppContextProvider} from './store/context';
 import WelcomeScreen from './screen/WelcomeScreen';
-
+import TabTestPuzzle from './screen/TabTestPuzzle';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={{headerShown:false}}>
+      <Tab.Screen name="TabTestPuzzle" component={TabTestPuzzle} />
+    </Tab.Navigator>
+  );
+};
 
 function App() {
   return (
     <AppContextProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
           <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
